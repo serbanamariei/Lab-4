249b7e0caf8ba3c16056c1bdf051ac11d79ee637
Serban Amariei-Hondrea <amarieiserban2@gmail.com>
2025-07-29 17:46:53 +0300

    Create functii.h

---
+++ problema-1.2/functii.h
@@ -0,0 +1,9 @@
+#ifndef F_H_
+#define F_H_
+
+int mediaAritmetica(int a, int b, int c);
+int mediaGeometrica(int a, int b, int c);
+int minim(int a, int b, int c);
+int maxim(int a, int b, int c);
+
+#endif
